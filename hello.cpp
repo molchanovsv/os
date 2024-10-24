b885511bd26b64e29bdbb2839f7d4ac542850eb0
molchanovSV <molchanov.sv@dvfu.ru>
2024-10-24 13:24:23 +1000

    640437

--- hello.cpp
+++ hello.cpp
@@ -2,10 +2,10 @@
 using namespace std;
 
 int main() {
-	int count = 3;
+	int count = 10;
 	
 	for (int i = 0; i < count; i++)
-		cout << "Hello world!" <<  "\n";
+		cout << "Hello world!!!" <<  "\n";
 	
 	return 0;
 }
